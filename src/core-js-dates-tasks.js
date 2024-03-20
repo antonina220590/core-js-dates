42888e2f7d621f1d36be2260b6371b6fafc42baf
antonina220590 <tonya2290@gmail.com>
2024-03-21 00:22:15 +0300

    feat: add solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,12 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const newDate = new Date(date);
+  const options = {
+    weekday: 'long',
+  };
+  return newDate.toLocaleDateString('en-US', options);
 }
 
 /**
